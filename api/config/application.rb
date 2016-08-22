8f8ce3752cff3c92e7063ef0604ac46c5d4aad9f
Austin Crane <amcrane1@wichita.edu>
2016-08-22 13:36:28 -0500

    trying to fix headers

--- api/config/application.rb
+++ api/config/application.rb
@@ -29,6 +29,7 @@ module InfoMessages
           :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
           :methods => :any
       end
+    end
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
